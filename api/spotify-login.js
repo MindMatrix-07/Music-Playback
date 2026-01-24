f7ffcfdf1efb6f7956b2b21583ab40cd72eb9610
gamero56789j <gamero56789j@gmail.com>
2026-01-24 06:30:10 +0000

    FIX: Use environment variable for Spotify redirect URI

--- api/spotify-login.js
+++ api/spotify-login.js
@@ -1,21 +1,18 @@
-// Spotify OAuth Login - Redirects user to Spotify authorization
-const CLIENT_ID = '1cc98da5d08742df809c8b0724725d0b';
-
-export default function handler(req, res) {
-    // Get the host from the request to build redirect URI dynamically
-    const protocol = req.headers['x-forwarded-proto'] || 'https';
-    const host = req.headers.host;
-    const redirectUri = `${protocol}://${host}/api/spotify-callback`;
-    
-    const scope = 'user-read-private user-read-email streaming user-read-playback-state user-modify-playback-state';
-    
-    const authUrl = 'https://accounts.spotify.com/authorize?' + new URLSearchParams({
-        response_type: 'code',
-        client_id: CLIENT_ID,
-        scope: scope,
-        redirect_uri: redirectUri,
-        show_dialog: true
-    }).toString();
-
-    res.redirect(authUrl);
-}
+      // Spotify OAuth Login - Redirects user to Spotify authorization
+      const CLIENT_ID = '1cc98da5d08742df809c8b0724725d0b';
+      
+      export default function handler(req, res) {
+          const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
+          
+          const scope = 'user-read-private user-read-email streaming user-read-playback-state user-modify-playback-state';
+          
+          const authUrl = 'https://accounts.spotify.com/authorize?' + new URLSearchParams({
+              response_type: 'code',
+              client_id: CLIENT_ID,
+              scope: scope,
+              redirect_uri: redirectUri,
+              show_dialog: true
+          }).toString();
+      
+          res.redirect(authUrl);
+      }
